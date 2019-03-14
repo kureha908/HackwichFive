ce2a7ab43f8305e4202e51b7c6c5336f7b73d5a6
Kureha <kureha@hawaii.edu>
2019-03-14 10:46:05 -1000

    Completed Hackwich #5 part 3&4

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -12,14 +12,33 @@ class ViewController: UIViewController {
 
     @IBOutlet var slider: UISlider!
     //setting the current view
-    var currentValue: Int = 0
+    var currentValue: Int = 50
+    var targetValue : Int = 0
+    
+    
+    
+    @IBOutlet var targetLabel: UILabel!
+    
+    
     @IBAction func sliderHasMoved(_ sender: Any) {
         print("The value of the slider is:\(slider.value) ")
         currentValue = lroundf(slider.value)
     }
     
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateTargetLabel()
+    }
+    
+    func updateTargetLabel(){
+        targetLabel.text = String(targetValue)
+    }
+    
+    
     @IBAction func myGuessButtonPressed(_ sender: Any) {
-        let message = "The value is: \(currentValue) "
+        let message = "The value is: \(currentValue) " + "\nThe target value is:\(targetValue)"
         //1. create alert view
         let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
         
@@ -31,12 +50,19 @@ class ViewController: UIViewController {
         
         //4. Present alertview on the screen
         present(alert, animated: true, completion: nil)
-    
         
+        startNewRound()
+
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+         currentValue = lroundf(slider.value)
+        //call start new
+        startNewRound()
+        let thumbnailImageNormal = #imageLiteral(resourceName: "SliderThumb-Normal")
+        slider.setThumbImage(thumbnailImageNormal, for: .normal)
+        
     }
 
 
